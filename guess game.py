ff50995320a9985582cf8545cf805ad28754c351
rishikbimal <84965614+rishikbimal@users.noreply.github.com>
2021-06-01 17:59:27 +1000

    initial commit

---
+++ guess game.py
@@ -0,0 +1,9 @@
+secret_number = 9
+guess_made = 0
+guess_limit = 3
+print ("Rules: This is a guessing game. You have three tries.")
+print ("The number will be a number between 1 and 20.")
+print ("Good Luck!")
+sleep(6)
+while guess_made < guess_limit
+    
---
+++ one.py
@@ -0,0 +1,37 @@
+#one.py
+operation = ""
+print(chr(27) + "[2J")
+
+while operation != "5": 
+
+   # print(chr(27) + "[2J")
+   
+    print("1. Addition")
+    print("2. Subtraction ")
+    print("3. Multiplication ")
+    print("4. Division ")
+    print("5. Exit ")
+    operation = input("Enter your option: ")
+    
+    if operation != "5":
+        first_number=float(input("First Number: "))
+        second_number=float(input("Second Number: "))
+        
+        if operation == "1":
+            sum = first_number + second_number
+            print (str(first_number) + " + " + str(second_number) + " = " + str(sum))
+        elif operation == "2":
+            diff = first_number - second_number
+            print (str(first_number) + " - " + str(second_number) + " = " + str(diff))
+        elif operation == "3":
+            multi = first_number * second_number
+            print (str(first_number) + " * " + str(second_number) + " = " + str(multi))
+        elif operation == "4":
+
+            if second_number == 0:
+                print ("Cannot divide by zero.")
+            else: 
+                divi = first_number / second_number
+                print (str(first_number) + " / " + str(second_number) + " = " + str(divi))
+        else:
+            print ("Invalid operation ")
